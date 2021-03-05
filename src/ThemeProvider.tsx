8ee49d2ff7353a8809632188548e34b49c37117e
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2021-03-05 16:08:57 +0100

    fix: 🐛 update ThemeProvider export

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import merge from 'deepmerge';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider as StyledProvider } from 'styled-components';
 
-import defaultTheme from './defaultTheme';
+import defaultTheme, { DefaultTheme } from './defaultTheme';
 import { GlobalStyles } from './GlobalStyle';
 
-function Provider({ theme = {}, children, ...props }) {
+interface ProviderProps {
+  /* Provider children */
+  children: React.ReactNode
+  /* Theme to inject in the provider */
+  theme: DefaultTheme
+}
+
+/**
+ * A `ThemeProvider` component which merges the default theme with custom theme props.
+ */
+export function ThemeProvider({ theme = defaultTheme, children, ...props }: ProviderProps) {
   const customTheme = React.useMemo(
     () => merge(defaultTheme, theme, { arrayMerge: (_, sourceArray) => sourceArray }),
     [theme]
   );
 
   return (
-    <ThemeProvider theme={customTheme} {...props}>
+    <StyledProvider theme={customTheme} {...props}>
       <GlobalStyles />
       {children}
-    </ThemeProvider>
+    </StyledProvider>
   );
 };
 
-Provider.displayName = 'ThemeProvider'
-
-export default Provider;
+ThemeProvider.displayName = 'ThemeProvider'
