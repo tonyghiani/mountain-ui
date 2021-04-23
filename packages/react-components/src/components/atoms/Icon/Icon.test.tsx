fbe1964d15e964b12053ff41db749ee151da0699
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2021-04-23 13:22:41 +0200

    fix(react-components): 🐛 remove dependency cycle

--- packages/react-components/src/components/atoms/Icon/Icon.test.tsx
+++ packages/react-components/src/components/atoms/Icon/Icon.test.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Mountains } from '@mountain-ui/icons';
 import { render } from 'mui-testing-tools';
 
 import Icon from './Icon';
@@ -8,7 +7,16 @@ describe.only('Icon', () => {
   it('should render correctly on mount', () => {
     const { container } = render(
       <Icon>
-        <Mountains />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 24 24'
+          width='1em'
+          height='1em'
+          preserveAspectRatio='xMidYMid meet'
+          fill='currentColor'
+        >
+          <path d='M21.85 17.47l-5-8a1 1 0 00-1.7 0l-1 1.63-3.29-5.6a1 1 0 00-1.72 0l-7 12A1 1 0 003 19h18a1 1 0 00.85-1.53zM10.45 17H4.74L10 8l2.93 5zm2.35 0l2.2-3.43 1-1.68L19.2 17z' />
+        </svg>
       </Icon>
     );
     expect(container.querySelector('svg')).toBeInTheDocument();
