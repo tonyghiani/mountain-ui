3a6c9011314286069dc7f4f913d90528ea0bc7e1
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2021-04-23 12:54:24 +0200

    refactor(react-components): 💡 update icon types

--- packages/react-components/src/components/atoms/Icon/Icon.tsx
+++ packages/react-components/src/components/atoms/Icon/Icon.tsx
@@ -4,16 +4,20 @@ import { system, typography, variant } from 'styled-system';
 
 import { BaseElementProps, styleProps } from '../../BaseElement';
 
-export type IconProps = BaseElementProps & {
+export type IconRef = HTMLButtonElement | HTMLSpanElement | null;
+
+export interface IconProps extends BaseElementProps {
   /* Variant version of the icon */
   variant?: 'icon' | 'button';
   /* Variant version of the icon */
+  onClick?: React.MouseEvent<IconRef>;
+  /* Transition on style changes */
+  withTransition?: boolean;
+  /* Variant version of the icon */
   children: React.ReactNode;
-};
+}
 
-export type Ref = HTMLButtonElement | HTMLSpanElement | null;
-
-const StyledIcon = styled.span<BaseElementProps>`
+const StyledIcon = styled.span<IconProps>`
   ${system({
     color: {
       properties: ['fill'],
@@ -28,10 +32,12 @@ const StyledIcon = styled.span<BaseElementProps>`
       button: {
         color: 'text.primary',
         border: 0,
-        bg: 'transparent'
+        bg: 'transparent',
+        cursor: 'pointer'
       }
     }
   })}
+  ${p => p.withTransition && '& > svg {transition: all .3s ease}'};
   ${typography}
   ${styleProps}
 `;
@@ -46,7 +52,7 @@ const VARIANT_TAGS = {
  */
 export const Icon = React.forwardRef(function Icon(
   { children, variant, ...props }: IconProps,
-  ref: React.Ref<Ref>
+  ref: React.Ref<IconRef>
 ) {
   return (
     <StyledIcon ref={ref} variant={variant} as={VARIANT_TAGS[variant]} {...props}>
@@ -56,7 +62,8 @@ export const Icon = React.forwardRef(function Icon(
 });
 
 Icon.defaultProps = {
-  variant: 'icon'
+  variant: 'icon',
+  withTransition: false
 };
 
 Icon.displayName = 'Icon';
