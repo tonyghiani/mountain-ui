50734ada675a39ff89e11016c17952d869ec1526
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2021-08-03 18:39:30 +0200

    fix(react-hooks): 🐛 add missing exports

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -1,8 +1,20 @@
 export { default as useBoolean } from './useBoolean';
 export * from './useBoolean';
 
+export { default as useDarkMode } from './useDarkMode';
+export * from './useDarkMode';
+
+export { default as useFontSize } from './useFontSize';
+export * from './useFontSize';
+
+export { default as useLocalStorage } from './useLocalStorage';
+export * from './useLocalStorage';
+
 export { default as useMediaQuery } from './useMediaQuery';
 export * from './useMediaQuery';
+
+export { default as usePrefersDarkMode } from './usePrefersDarkMode';
+export * from './usePrefersDarkMode';
 
 export { default as useToggle } from './useToggle';
 export * from './useToggle';
