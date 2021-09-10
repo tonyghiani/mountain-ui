ffef0b258ea0619ba43688512bf54965a4807cc0
Marco Antonio Ghiani <marco.ghiani@adevinta.com>
2021-09-10 16:46:12 +0200

    feat(react-hooks): 🎸 add useEventListener hook

---
+++ packages/react-hooks/src/useEventListener/useEventListener.ts
@@ -0,0 +1,63 @@
+/*
+ * Extended from Gabe Ragland implementation https://usehooks.com/useEventListener/
+ * to accept multiple events with the same handler and listener options
+ */
+import React, { useEffect, useRef } from 'react';
+import { hasWindow } from '@mountain-ui/utils';
+
+export type Events = string | string[];
+export type Target =
+  | null
+  | undefined
+  | Window
+  | HTMLElement
+  | React.MutableRefObject<HTMLElement | null | undefined>;
+
+function getElement(target: Target) {
+  const targetIsRef = 'current' in target;
+  return targetIsRef ? target.current : target;
+}
+
+function useEventListener(
+  events: Events,
+  handler: EventListener,
+  target: Target = hasWindow() ? window : null,
+  options?: boolean | AddEventListenerOptions
+): void {
+  // Create a list of events if a single string is passed
+  const eventList = Array.isArray(events) ? events : [events];
+  const serializedEventList = JSON.stringify(eventList);
+  const serializedOptions = JSON.stringify(options);
+
+  // Create a ref that stores the handler
+  const savedHandler = useRef(handler);
+
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler ...
+  // ... without us needing to pass it in effect deps array ...
+  // ... and potentially cause effect to re-run every render.
+  // Search for "Latest Ref pattern" to know more about the topic.
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    // Use window if no element is passed, otherwise default to null in SSR
+    const element = getElement(target);
+    // Make sure element supports addEventListener
+    const isSupported = element && element.addEventListener;
+    if (!isSupported) return;
+
+    const listener = (event: Event) => savedHandler.current && savedHandler.current(event);
+
+    // Add event listener
+    eventList.forEach(event => element.addEventListener(event, listener, options));
+
+    // Remove event listener on cleanup
+    return () => {
+      eventList.forEach(event => element.removeEventListener(event, listener, options));
+    };
+  }, [serializedEventList, target, serializedOptions]);
+}
+
+export default useEventListener;
