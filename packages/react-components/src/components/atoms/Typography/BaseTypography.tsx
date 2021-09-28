6b1ec33aa24cbd02eb231ecb9f8f7d76a5dfec97
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2021-09-28 18:46:58 +0200

    fix(react-components): 🐛 add missing gradient property typograp

--- packages/react-components/src/components/atoms/Typography/BaseTypography.tsx
+++ packages/react-components/src/components/atoms/Typography/BaseTypography.tsx
@@ -1,22 +1,33 @@
 import styled, { css } from 'styled-components';
-import { compose, system, textStyle, TextStyleProps, typography, TypographyProps } from 'styled-system'
+import {
+  compose,
+  system,
+  textStyle,
+  TextStyleProps,
+  typography,
+  TypographyProps
+} from 'styled-system';
 
 import { BaseElement, BaseElementProps } from '../../BaseElement';
 
-const textStyles = compose(typography, textStyle)
+const textStyles = compose(typography, textStyle);
 
-export type BaseTypographyProps = BaseElementProps & TypographyProps & TextStyleProps & {
-  /* Text should be uncopyable */
-  uncopyable?: boolean;
-  /* Text should be bold */
-  strong?: boolean;
-  /* Text should be wrapped in ellipsis */
-  wrapped?: boolean;
-  /* Text should be clamped to specified line count */
-  lineClamp?: number;
-  /* Text should be underlined */
-  underline?: boolean;
-}
+export type BaseTypographyProps = BaseElementProps &
+  TypographyProps &
+  TextStyleProps & {
+    /* Text should be uncopyable */
+    uncopyable?: boolean;
+    /* Text should be bold */
+    strong?: boolean;
+    /* Text should be wrapped in ellipsis */
+    wrapped?: boolean;
+    /* Text should be clamped to specified line count */
+    lineClamp?: number;
+    /* Text should be underlined */
+    underline?: boolean;
+    /* Text have gradient color */
+    gradient?: string;
+  };
 
 const wrapped = css`
   white-space: nowrap;
@@ -42,18 +53,24 @@ const uncopyable = css`
   user-select: none;
 `;
 
+const gradientColor = css`
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
 /**
- * TODO: add component description headline
+ * The `BaseTypography` component works as the first brick to compose more specific text components
  */
-const BaseTypography = styled(BaseElement) <BaseTypographyProps>`
+const BaseTypography = styled(BaseElement)<BaseTypographyProps>`
   ${textStyles}
   ${system({ userSelect: true })}
+  ${p => p.gradient && `background: linear-gradient(${p.gradient}); ${gradientColor} `}
   ${p => p.lineClamp && `${lineClamp}-webkit-line-clamp: ${p.lineClamp};`}
-  ${p => p.wrapped && wrapped}
   ${p => p.strong && strong}
-  ${p => p.underline && underline}
   ${p => p.uncopyable && uncopyable}
-`
+  ${p => p.underline && underline}
+  ${p => p.wrapped && wrapped}
+`;
 BaseTypography.displayName = 'BaseTypography';
 
-export default BaseTypography
+export default BaseTypography;
