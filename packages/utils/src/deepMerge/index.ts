f223b5d27505c975c2ad6e2f0f2cc2d1f44b6fda
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2023-10-20 15:11:39 +0200

    refactor(utils): 💡 switch to deepMerge

--- packages/utils/src/deepMerge/index.ts
+++ packages/utils/src/deepMerge/index.ts
@@ -1,1 +1,1 @@
-export { default } from './deepMerge';
+export { default } from 'deepmerge';
--- packages/utils/src/isFunction/isFunction.ts
+++ packages/utils/src/isFunction/isFunction.ts
@@ -1,10 +1,11 @@
+type AnyFunction = (...args: unknown[]) => unknown;
 /**
  * Type guard to check if a value is a function.
  *
  * @param {unknown} value - The value to check.
  * @returns {value is Function} True if the value is a function, otherwise false.
  */
-function isFunction(arg: unknown): arg is Function {
+function isFunction<Fn>(arg: Fn): arg is Extract<Fn, AnyFunction> {
   return typeof arg === 'function';
 }
 
